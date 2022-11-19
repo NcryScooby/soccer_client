689df15fadeca6f356769616d36e14e5b8ccc725
Fabrício <fabricio_c_anelli@hotmail.com>
2022-11-19 15:45:38 -0300

    #3 - Organizing the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// remove [vite] connected... message
-process.env.VITE_SUPPRESS_CONNECT_MESSAGE = "true";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
